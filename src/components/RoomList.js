170de8af5456c1317735acd583cc1bdc95f1838c
David Hirst <34040303+drwhirstyburgers@users.noreply.github.com>
2018-03-18 16:46:47 -0400

    add room function add

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -5,7 +5,8 @@ class RoomList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      rooms: []
+      rooms: [],
+      newRoom: ''
     };
     this.roomsRef = this.props.firebase.database().ref('rooms');
 
@@ -19,15 +20,29 @@ class RoomList extends Component {
     });
   }
 
+  handleChange(e) {
+    this.setState({ newRoom: e.target.value });
+  }
+
+  createRoom(newRoom) {
+    this.roomsRef.push({
+      name: newRoom
+    });
+  }
+
   render() {
     return (
       <section className="RoomList">
         <h2>Bloc Chat</h2>
         <ul id="room-list">
           {this.state.rooms.map( room =>
-          <li key={room.key}>{ room.name }</li>
+          <li>{ room.name }</li>
         )}
         </ul>
+        <form onSubmit={ (e) => { e.preventDefault; this.createRoom(this.state.newRoom) } } className="create-room">
+          <input type="text" value={ this.state.newRoom } onChange={ this.handleChange.bind(this) } placeholder="add a room!" />
+          <input type="submit" value="add" />
+        </form>
       </section>
     );
   }
