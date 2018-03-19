220cdcc2e2eb3ce1c5b338c979558a8dcf02daf2
David Hirst <34040303+drwhirstyburgers@users.noreply.github.com>
2018-03-19 15:16:36 -0400

    MessageList component created and firebase passed as prop

---
+++ src/components/MessageList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MessageList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+
+    )
+  }
+
+export default RoomList;
